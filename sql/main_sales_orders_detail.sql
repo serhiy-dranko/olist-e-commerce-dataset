5959e2d466b6ed2655fa8d62fffb483e8a209447
Serhiy Dranko <163868717+serhiy-dranko@users.noreply.github.com>
2026-08-12 10:32:08 +0200

    Enhance main_sales_orders_detail with category and state totals
    Added category and state totals for pie chart analysis.

--- sql/main_sales_orders_detail.sql
+++ sql/main_sales_orders_detail.sql
@@ -1,3 +1,4 @@
+CREATE OR REPLACE TABLE main_sales_orders_detail AS
 WITH payments_per_order AS (
     SELECT
         order_id,
@@ -22,8 +23,34 @@ SELECT
         ORDER BY review_creation_date DESC
     ) AS rn
 FROM olist_order_reviews_dataset
-)  
-
+),
+-- For Top 10 pCat pie chart
+category_totals AS (
+    SELECT
+        t.product_category_name_english AS category,
+        SUM(oi.price) + SUM(oi.freight_value) AS total_val,
+        RANK() OVER (ORDER BY SUM(oi.price) + SUM(oi.freight_value) DESC) AS category_rank
+    FROM olist_order_items_dataset oi
+    LEFT JOIN olist_products_dataset p
+        ON oi.product_id = p.product_id
+    LEFT JOIN product_category_name_translation t
+        ON p.product_category_name = t.product_category_name
+    GROUP BY t.product_category_name_english
+),
+-- For Top 10 State pie chart
+state_totals AS (
+    SELECT
+        c.customer_state AS state,
+        SUM(oi.price) + SUM(oi.freight_value) AS total_val,
+        RANK() OVER (ORDER BY SUM(oi.price) + SUM(oi.freight_value) DESC) AS state_rank
+    FROM olist_order_items_dataset oi
+    LEFT JOIN olist_orders_dataset o
+        ON oi.order_id = o.order_id
+    LEFT JOIN olist_customers_dataset c
+        ON o.customer_id = c.customer_id
+    GROUP BY c.customer_state
+ )
+  
 SELECT
     o.order_id,
     o.customer_id,
@@ -38,10 +65,12 @@ SELECT
     oi.seller_id,
     oi.price,
     oi.freight_value,
+    CASE WHEN st.state_rank <= 10 THEN c.customer_state ELSE 'Other' END AS state_grouped,-- For Top 10 State pie chart
     c.customer_state,
     c.customer_city,
     p.product_category_name,
-    t.product_category_name_english,
+    COALESCE(t.product_category_name_english,'Unknown') AS product_category_name_english,
+    CASE WHEN ct.category_rank <= 10 THEN t.product_category_name_english ELSE 'other' END AS pCat_grouped,-- For Top 10 pCat pie chart
     r.review_score,
     r.review_comment_title,
     r.review_comment_message
@@ -57,6 +86,10 @@ LEFT JOIN product_category_name_translation t
     ON p.product_category_name = t.product_category_name
 LEFT JOIN payments_per_order pp
     ON o.order_id = pp.order_id
+LEFT JOIN category_totals ct -- For Top 10 pCat pie chart
+    ON t.product_category_name_english = ct.category
+LEFT JOIN state_totals st -- For Top 10 State pie chart
+    ON c.customer_state = st.state
 LEFT JOIN last_review_per_order r
     ON o.order_id = r.order_id
     AND r.rn = 1
